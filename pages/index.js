0398066fa20ec9f88562ced7e0cfc2f0f794b5d1
sistiri <sistiri@yahoo.com>
2022-04-21 10:25:13 +0200

    Creating Pages

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,7 @@
 // our-domain.com/
+
+function HomePage() {
+  return <h1>HomePage</h1>;
+}
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,7 @@
+// our-domain.com/news
+
+function NewsPage() {
+  return <h1>NewsPage</h1>;
+}
+
+export default NewsPage;
