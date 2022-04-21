f422f24d390a0ca677558f6aec34e7298af22a38
sistiri <sistiri@yahoo.com>
2022-04-21 10:50:22 +0200

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,23 @@
 // our-domain.com/news
+import Link from 'next/link'
+import { Fragment } from "react";
 
 function NewsPage() {
-    return <h1>NewsPage</h1>;
-  }
-  
-  export default NewsPage;
+  return (
+    <Fragment>
+      <h1>NewsPage</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-frame-work">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>
+          <a href="">Something Else</a>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
